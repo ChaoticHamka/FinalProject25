da3683e5b4d310f23ed0e2ddfe0bec471f6d8995
Юлия <chaotic.hamka@gmail.com>
2025-07-22 20:55:23 +0300

    refactor BaseApi

--- src/main/java/by/itacademy/radzetskaya/ria/api/utils/BaseApi.java
+++ src/main/java/by/itacademy/radzetskaya/ria/api/utils/BaseApi.java
@@ -1,11 +1,11 @@
-package by.itacademy.radzetskaya.ria.API.utils;
+package by.itacademy.radzetskaya.ria.api.utils;
 
 import by.itacademy.radzetskaya.ria.Base.TestUtils;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
 
-public class BaseAPI extends TestUtils {
+public class BaseApi extends TestUtils {
 
     protected Response response;
 
@@ -13,23 +13,14 @@ public class BaseAPI extends TestUtils {
         return response.statusCode();
     }
 
-    public Response getResponse() {
-        return response;
-    }
-
     public JsonPath getResponseAsJSON() {
         String jsonText = response.getBody().asString().replaceAll("<[^>]+>", "").trim();
-        JsonPath json = new JsonPath(jsonText);
-        return json;
-    }
-
-    public void logResponse() {
-        response.then().log().all();
+        return new JsonPath(jsonText);
     }
 
     @AfterEach
     public void tearDown(){
-        LogAPI.endTest();
+        LogApi.endTest();
     }
 
 }
